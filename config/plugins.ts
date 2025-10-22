a39308cf2cfdeabd07cf26a55281144234e7acf7
Ammara <amarsal2743@gmail.com>
2025-10-22 18:02:09 -0400

    Add JWT secret config for users-permissions plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,8 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  // Default plugin configurations
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET', 'defaultJwtSecret'),
+    },
+  },
+});
