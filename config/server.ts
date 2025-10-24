de8054966488bb8c11429488af60b5ac4b97b9d9
Ammara <amarsal2743@gmail.com>
2025-10-24 19:15:32 -0400

    update

--- config/server.ts
+++ config/server.ts
@@ -1,10 +1,9 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://blog-website-1-to2c.onrender.com/admin'),
+  url: env('HOST_URL', 'https://blog-website-1-to2c.onrender.com/admin'),
   app: {
     keys: env.array('APP_KEYS'),
-    proxy: true, // important to trust Render's HTTPS proxy
   },
    proxy: true, // <-- critical for Strapi v4 behind reverse proxy
   
